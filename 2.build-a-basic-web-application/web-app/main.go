b3c02e881efd4898efd5386b17ef75a03fa3b1b0
bartal læarsson <bartal.baek@gmail.com>
2025-08-31 23:20:30 +0100

    SOMETHING

--- 2.build-a-basic-web-application/web-app/main.go
+++ 2.build-a-basic-web-application/web-app/main.go
@@ -1,53 +1,37 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 const portNumber = ":8080"
 
-// Home is the home page handler fuck
-// Another line
+// Home is the Home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
+	renderTemplate(w, "home.page.tmpl.html")
 }
 
-// About is the about page handler
+// About is the About page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(2, 2)
-	_, _ = fmt.Fprintf(w, "This is the about page and 2 + 2 is: %d", sum)
+	renderTemplate(w, "about.page.tmpl.html")
 }
 
-func divide(w http.ResponseWriter, r *http.Request) {
-	f, err := divideValues(100.00, 10.0)
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
-		fmt.Fprint(w, "Cannot divide by 0")
+		fmt.Println("Error passing template", err)
 		return
 	}
-	fmt.Fprintf(w, "%f divided by %f is %f", 100.0, 10.0, f)
-}
 
-func divideValues(x, y float32) (float32, error) {
-	if y <= 0 {
-		err := errors.New("Cannot divide by o")
-		return 0, err
-	}
-	result := x / y
-	return result, nil
-}
-
-// addValues add two numbers together and return the result
-func addValues(x, y int) int {
-	return x + y
 }
 
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", divide)
 
-	fmt.Printf("Starting application on port number %s\n", portNumber[1:])
-	_ = http.ListenAndServe(portNumber, nil)
+	fmt.Printf("The applicaiton is running on port: %s", portNumber)
+	http.ListenAndServe(portNumber, nil)
 }
